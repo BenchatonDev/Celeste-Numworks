69932a7422553bd46214642465f879c8a6a11bb8
BenchatonDev <benchaton.annimation@gmail.com>
2025-10-18 21:59:03 +0200

    It's ALIVE !
    She runs ! with a few (lot of) problems, only visual though

--- src/main.cpp
+++ src/main.cpp
@@ -25,36 +25,7 @@ int main(void) {
     while (running) { // You can always exit by double pressing home so \(°-°)/
         if (state.keyDown(Keyboard::Key::Home)) { running = false; }
 
-        // These work fine
-        //emulator(CELESTE_P8_CIRCFILL, 20, 20, 2, 12);
-        //emulator(CELESTE_P8_CIRCFILL, 40, 40, 3, 10);
-
-        // Works too
-        //emulator(CELESTE_P8_PAL, 8, 11);
-        //emulator(CELESTE_P8_SPR, 5, 40, 40, 1, 1, false, false);
-        //emulator(CELESTE_P8_PAL_RESET);
-        //emulator(CELESTE_P8_SPR, 20, 20, 20, 1, 1, false, true);
-
-        // Printing : OK
-        //emulator(CELESTE_P8_PRINT, "Hello World !", 20, 20, 7);
-        //emulator(CELESTE_P8_PRINT, "This works nicely !", 40, 40, 7);
-
-        // Map : OK (at least on num 0, other untested)
-        //emulator(CELESTE_P8_MAP, 0 * 16, 0 * 16, 0, 0, 16, 16, 4);
-        // the value of int off need to be 0 to not have flickering
-        //emulator(CELESTE_P8_MAP, 0 * 16, 0 * 16, 0, 0, 16, 16, 2);
-        //emulator(CELESTE_P8_MAP, 0 * 16,0 * 16, 0, 0, 16, 16, 8);
-
-        // Line and rect
-        emulator(CELESTE_P8_LINE, 50, 50, 150, 128, 12);
-        emulator(CELESTE_P8_RECTFILL, -4, -4, 2, 2, 7);
-        emulator(CELESTE_P8_RECTFILL, 129, 129, 7, 4, 7);
-        emulator(CELESTE_P8_RECTFILL, -1, -3, 2, 5, 7);
-        emulator(CELESTE_P8_RECTFILL, 128, 112, 6, 5, 7);
-
-        // Maybe flickering is because we're rendering to fast ?
-        //Timing::msleep(1000/30);
-        // Nope not the actuall problem
+        gameMain();
     }
 
     free(gameState);
