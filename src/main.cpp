4461b7ff2ae7196cd7021437367151f115c47755
BenchatonDev <benchaton.annimation@gmail.com>
2025-10-19 17:54:46 +0200

    Nah I didn't save file 😭
    Didn't want to commit that test code copy pasted from stack overflow but ig I did it anyways

--- src/main.cpp
+++ src/main.cpp
@@ -4,11 +4,6 @@
 // The actual game port THX Lemon :)
 #include "translation.hpp"
 
-volatile uint64_t g_millisecond_counter = 0;
-extern "C" void SysTick_Handler(void) { g_millisecond_counter++; }
-
-uint64_t my_millis() { return g_millisecond_counter; }
-
 // Saying the namespaces we'll use on a
 // semi regularly so it's less of a
 // hassle to write common functions etc
@@ -22,26 +17,13 @@ int main(void) {
     Display::pushRectUniform(Screen::Rect, 0x000000);
     emuInit();
 
-    //Celeste_P8_update();
-    //Celeste_P8_draw();
-
     while (running) { // You can always exit by double pressing home so \(°-°)/
-        auto frameStart = my_millis();
-        
         if (state.keyDown(Keyboard::Key::Home)) { running = false; }
 
         gameMain();
         Display::pushRectUniform(Rect(0,0,32,240), 0x000000);
         Display::pushRectUniform(Rect(288,0,32,240), 0x000000);
-
-        auto frameTime = my_millis() - frameStart;
-
-        if (frameTime < targetDeltaTime) {
-            auto sleepTime = targetDeltaTime - frameTime;
-            Timing::msleep(static_cast<unsigned int>(sleepTime));
-        }
     }
-
     free(gameState);
 
     return 0;
